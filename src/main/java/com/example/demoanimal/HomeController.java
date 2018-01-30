546aa5b571259c8d9f9788a193a648dba48de9d9
Christina71 <mariachristina71@yahoo.com>
2018-01-30 09:29:54 -0500

    Initial commit

---
+++ src/main/java/com/example/demoanimal/HomeController.java
@@ -0,0 +1,34 @@
+package com.example.demoanimal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+         String output;
+        @RequestMapping("/animal")
+        public String main() {
+            output="";
+            Animal a = new Animal();
+            print(a.eat());
+            print(a.sleep());
+
+            Cat c = new Cat();
+            print(c.eat());
+            print(c.sleep());
+            print(c.purr());
+
+
+            Bird b = new Bird();
+            print(b.eat());
+            print(b.sleep());
+            print(b.fly());
+            return output;
+
+        }
+        private void print(String s){
+           // System.out.println(s);
+            output+=s;
+        }
+    }
+
